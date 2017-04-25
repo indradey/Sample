2ec31cd40453511e3d8227d3a55187bb8d79c7d6
indradey <deyindranil@outlook.com>
2017-04-25 22:21:40 +0530

    commit

---
+++ MyFirstNode/MyFirstNode/helloExpress.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var server = express();
+
+server.set('views',__dirname);
+server.get('/', function (req,res) {
+    //res.write();
+    //res.end('Hello Express !!');
+    //res.sendFile('C:\Users\Indranil\documents\visual studio 2017\Projects\MyFirstNode\MyFirstNode\Index.html');
+    res.sendFile('index.html', { root: __dirname });
+});
+
+server.get('/Render', function (req, resp) {
+    //Rendering a page
+    resp.render('dynamic.ejs');
+});
+
+
+server.get('/Json', function (req, resp) {
+    //Passing a json object
+    resp.setHeader('Content-Type', 'application/json');
+    resp.send(JSON.stringify({ "Name": "Indranil", "rollnumber": "115032" }));
+});
+
+server.listen(8090);
---
+++ MyFirstNode/MyFirstNode/server.js
@@ -0,0 +1,23 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+var fs = require("fs");
+var data = fs.readFileSync('Note.txt'); 
+
+var events = require("events");
+var eventemitter = new events.EventEmitter();
+
+var eventHandler = function () {
+    eventemitter.emit('This is from first place');
+};
+
+eventemitter.on("eventName", eventHandler);
+eventemitter.emit('This is from the event');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(data.toString());
+    res.end('Hello World\n');
+}).listen(port);
+
