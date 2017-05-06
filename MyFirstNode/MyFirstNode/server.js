6451d4e843a6501146fd07ec051d5d4a2f48dc0f
= <=>
2017-05-06 19:32:08 +0530

    commited the changes

--- MyFirstNode/MyFirstNode/server.js
+++ MyFirstNode/MyFirstNode/server.js
@@ -1,6 +1,6 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1336;
 
 var fs = require("fs");
 var data = fs.readFileSync('Note.txt'); 
@@ -16,7 +16,7 @@ eventemitter.on("eventName", eventHandler);
 eventemitter.emit('This is from the event');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/json' });
     res.write(data.toString());
     res.end('Hello World\n');
 }).listen(port);
